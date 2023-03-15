2c15c0b3434a9db7cde81ae645a3f033df285e30
Apefa883 <apefa883@mtroyal.ca>
2023-03-14 21:32:36 -0600

    Added unit tests

--- src/testing/unittests.java
+++
@@ -1,6 +0,0 @@
-package testing;
-
-public class unittests {
-	//This class contains snippets of code from elsewhere transplanted here for the purpose of testing.
-	//Don't worry about it for now.
-}
